502b05794f01ce438d1688b480a00e6b2f174b79
maulana677 <maull0776@gmail.com>
2023-12-28 20:29:52 +0700

    menampilkan data portfolio di frontend

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\About;
+use App\Models\Category;
 use App\Models\Hero;
+use App\Models\PortfolioItem;
+use App\Models\PortfolioSectionSetting;
 use App\Models\Service;
 use App\Models\TyperTitle;
 use Illuminate\Http\Request;
@@ -17,13 +20,19 @@ class HomeController extends Controller
         $typerTitles = TyperTitle::all();
         $services = Service::all();
         $about = About::first();
+        $portfolioTitle = PortfolioSectionSetting::first();
+        $portfolioCategories = Category::all();
+        $portfolioItems = PortfolioItem::all();
         return view(
             'frontend.home',
             compact(
                 'hero',
                 'typerTitles',
                 'services',
-                'about'
+                'about',
+                'portfolioTitle',
+                'portfolioCategories',
+                'portfolioItems'
             )
         );
     }
