15a8b95c9ccc3b005b6376f79419381c5ba5e455
maulana677 <maull0776@gmail.com>
2023-12-31 08:09:45 +0700

    menambahkan fitur pembaruan dan penghapusan blog category

--- app/Http/Controllers/Admin/BlogCategoryController.php
+++ app/Http/Controllers/Admin/BlogCategoryController.php
@@ -71,6 +71,8 @@ class BlogCategoryController extends Controller
      */
     public function edit($id)
     {
+        $category = BlogCategory::findOrFail($id);
+        return view('admin.blog-category.edit', compact('category'));
     }
 
     /**
@@ -82,6 +84,18 @@ class BlogCategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'name' => ['required', 'max:200']
+        ]);
+
+        $category = BlogCategory::findOrFail($id);
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $category->save();
+
+        session()->flash("success", "Data Updated Successfully");
+
+        return redirect()->route('admin.blog-category.index');
     }
 
     /**
@@ -92,5 +106,7 @@ class BlogCategoryController extends Controller
      */
     public function destroy($id)
     {
+        $category = BlogCategory::findOrFail($id);
+        $category->delete();
     }
 }
